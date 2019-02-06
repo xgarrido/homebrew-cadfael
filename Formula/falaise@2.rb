a9d1467b1304cf6268a5bb73c7755a4dab9086be
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2019-02-06 12:07:20 +0000

    Remove no-longer support `needs: cxx11`
    Additional fixes on top of PR #72

--- Formula/falaise@2.rb
+++ Formula/falaise@2.rb
@@ -9,7 +9,6 @@ class FalaiseAT2 < Formula
 
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
-
   # Bayeux dependency pulls in all additional deps of Falaise at present
   depends_on "supernemo-dbd/cadfael/bayeux@2"
 
