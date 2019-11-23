8ab1408699eee503befcd2caee6c80e1d2d9b040
Xavier Garrido <xavier.garrido@gmail.com>
2019-11-23 22:09:56 +0100

    remove patch

--- Formula/clhep.rb
+++ Formula/clhep.rb
@@ -6,9 +6,6 @@ class Clhep < Formula
   stable do
     url "http://proj-clhep.web.cern.ch/proj-clhep/DISTRIBUTION/tarFiles/clhep-2.1.3.1.tgz"
     sha256 "5d3e45b39a861731fe3a532bb1426353bf62b54c7b90ecf268827e50f925642b"
-
-    # Patch for clang compatibility, adapted from MacPorts
-    patch :DATA
   end
 
   devel do
@@ -46,20 +43,3 @@ class Clhep < Formula
 end
 
 __END__
-# https://savannah.cern.ch/bugs/?104110
-# https://trac.macports.org/ticket/42841
-#
---- a/CLHEP/Matrix/src/Vector.cc.orig
-+++ b/CLHEP/Matrix/src/Vector.cc
-@@ -114,9 +114,9 @@ HepVector::HepVector(const HepMatrix &hm1)
-
- // trivial methods
-
--inline int HepVector::num_row() const {return nrow;}
--inline int HepVector::num_size() const {return nrow;}
--inline int HepVector::num_col() const { return 1; }
-+int HepVector::num_row() const {return nrow;}
-+int HepVector::num_size() const {return nrow;}
-+int HepVector::num_col() const { return 1; }
-
- // operator()
